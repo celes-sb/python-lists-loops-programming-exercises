731479b903ba85ac67bff9ae70f6f26992a4b663
Celeste <cnyx7@proton.me>
2023-08-18 00:42:59 +0000

    ex2.5 to ex8

--- .learn/resets/08-Delete_element/app.py
+++ .learn/resets/08-Delete_element/app.py
@@ -2,13 +2,8 @@ people = ['juan','ana','michelle','daniella','stefany','lucy','barak']
 
 #Your code go here:
 def deletePerson(person_name):
-    new_people = []
     #Your code go here:
-    for person in people:
-        if person != person_name:
-            new_people.append(person)
-    return new_people
-            
+    
 print(deletePerson("daniella"))
 print(deletePerson("juan"))
 print(deletePerson("emilio"))
